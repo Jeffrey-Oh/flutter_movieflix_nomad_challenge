b23875aeb4987ffffc9a722437e6861d8123e096
오태현 <jeffrey@kai-i.com>
2023-04-16 21:46:11 +0900

    movieflix done

--- lib/main.dart
+++ lib/main.dart
@@ -29,37 +29,65 @@ class Movieflix extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
-  Future<List<Popular>> populars = ApiService.getPopular();
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isLoading = true;
+  List<MovieInfo> populars = [];
+  List<MovieInfo> playing = [];
+  List<MovieInfo> coming = [];
+
+  void fetchData() async {
+    populars = await ApiService.getMovieInfos("popular");
+    playing = await ApiService.getMovieInfos("playing");
+    coming = await ApiService.getMovieInfos("coming");
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: FutureBuilder(
-          future: populars,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 15),
-                      child: Text(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              if (isLoading)
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 50,
+                    child: Image.asset("assets/images/logo.png"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Opacity(
+                      opacity: isLoading ? 0 : 1,
+                      child: const Text(
                         'Popular Movies',
                         style: TextStyle(
                           fontSize: 24,
@@ -67,26 +95,184 @@ class HomeScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                    const SizedBox(
-                      height: 20,
+                  ),
+                  const SizedBox(height: 5),
+                  makeListViewBuilder(
+                    list: populars,
+                    listIndex: 0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Opacity(
+                      opacity: isLoading ? 0 : 1,
+                      child: const Text(
+                        'Now in Cinemas',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
                     ),
-                    Expanded(
-                      child: makePopularList(snapshot),
+                  ),
+                  const SizedBox(height: 5),
+                  makeListViewBuilder(
+                    list: playing,
+                    listIndex: 1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Opacity(
+                      opacity: isLoading ? 0 : 1,
+                      child: const Text(
+                        'Coming soon',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
                     ),
-                  ],
-                ),
-              );
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
+                  ),
+                  const SizedBox(height: 5),
+                  makeListViewBuilder(
+                    list: coming,
+                    listIndex: 2,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  ListView makePopularList(AsyncSnapshot<List<Popular>> snapshot) {
+  SizedBox makeListViewBuilder({
+    required List<MovieInfo> list,
+    required int listIndex,
+  }) {
+    return SizedBox(
+      height: listIndex == 0 ? 400 : 160,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          var item = list[index];
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: listIndex == 0 ? 400 : 160,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailScreen(
+                          id: item.id,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        width: listIndex == 0 ? 200 : 140,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 5,
+                              offset: const Offset(5, 5),
+                              color: Colors.black.withOpacity(0.5),
+                            )
+                          ],
+                        ),
+                        child: Image.network(
+                          listIndex == 0
+                              ? ApiService.imgUrl + item.posterPath
+                              : ApiService.imgUrl + (item.backdropPath ?? ''),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      SizedBox(
+                        width: listIndex == 0 ? 230 : 160,
+                        child: listIndex == 0
+                            ? ListTile(
+                                title: Text(
+                                  item.title,
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                trailing: Container(
+                                  width: 55,
+                                  padding: const EdgeInsets.all(5),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Colors.black.withOpacity(0.5),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      if (item.voteAverage <= 2.5)
+                                        const Text('🥚'),
+                                      if (item.voteAverage > 2.5 &&
+                                          item.voteAverage <= 5)
+                                        const Text('🐣'),
+                                      if (item.voteAverage > 5 &&
+                                          item.voteAverage <= 7.5)
+                                        const Text('🐤'),
+                                      if (item.voteAverage > 7.5)
+                                        const Text('🐥'),
+                                      const SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text(
+                                        item.voteAverage is int
+                                            ? '${item.voteAverage}.0'
+                                            : '${item.voteAverage}',
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            : Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 5),
+                                child: Text(
+                                  textAlign: TextAlign.center,
+                                  item.title,
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  ListView makeMoviesInfoList({
+    required AsyncSnapshot<List<MovieInfo>> snapshot,
+    required int listIndex,
+  }) {
     return ListView.builder(
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
@@ -106,7 +292,7 @@ class HomeScreen extends StatelessWidget {
             children: [
               Container(
                 clipBehavior: Clip.hardEdge,
-                width: 200,
+                width: listIndex == 0 ? 200 : 140,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
                   boxShadow: [
@@ -118,54 +304,71 @@ class HomeScreen extends StatelessWidget {
                   ],
                 ),
                 child: Image.network(
-                  ApiService.imgUrl + item.posterPath,
+                  listIndex == 0
+                      ? ApiService.imgUrl + item.posterPath
+                      : ApiService.imgUrl + (item.backdropPath ?? ''),
                 ),
               ),
               const SizedBox(
                 height: 5,
               ),
               SizedBox(
-                width: 230,
-                child: ListTile(
-                  title: Text(
-                    item.title,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  trailing: Container(
-                    width: 55,
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.black.withOpacity(0.5),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        if (item.voteAverage <= 2.5) const Text('🥚'),
-                        if (item.voteAverage > 2.5 && item.voteAverage <= 5)
-                          const Text('🐣'),
-                        if (item.voteAverage > 5 && item.voteAverage <= 7.5)
-                          const Text('🐤'),
-                        if (item.voteAverage > 7.5) const Text('🐥'),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          item.voteAverage is int
-                              ? '${item.voteAverage}.0'
-                              : '${item.voteAverage}',
+                width: listIndex == 0 ? 230 : 160,
+                child: listIndex == 0
+                    ? ListTile(
+                        title: Text(
+                          item.title,
                           style: const TextStyle(
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                ),
+                        trailing: Container(
+                          width: 55,
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              if (item.voteAverage <= 2.5) const Text('🥚'),
+                              if (item.voteAverage > 2.5 &&
+                                  item.voteAverage <= 5)
+                                const Text('🐣'),
+                              if (item.voteAverage > 5 &&
+                                  item.voteAverage <= 7.5)
+                                const Text('🐤'),
+                              if (item.voteAverage > 7.5) const Text('🐥'),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                item.voteAverage is int
+                                    ? '${item.voteAverage}.0'
+                                    : '${item.voteAverage}',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 5),
+                        child: Text(
+                          textAlign: TextAlign.center,
+                          item.title,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
               ),
             ],
           ),
@@ -184,7 +387,7 @@ class DetailScreen extends StatelessWidget {
 
   final int id;
 
-  late Future<Detail> detail = ApiService.getDetail(id);
+  late final Future<Detail> detail = ApiService.getDetail(id);
 
   @override
   Widget build(BuildContext context) {
@@ -264,21 +467,21 @@ class DetailScreen extends StatelessWidget {
               const SizedBox(height: 20),
               makeRuntimeAndGenres(item.runtime, item.genres),
               const SizedBox(height: 40),
+              const Text(
+                'Overview',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 10),
               SizedBox(
-                height: 150,
+                height: 120,
                 child: SingleChildScrollView(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Text(
-                        'Overview',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
                       Text(
                         item.overview,
                         style: const TextStyle(
@@ -383,15 +586,17 @@ class DetailScreen extends StatelessWidget {
   }
 }
 
-class Popular {
+class MovieInfo {
   final int id;
-  final String posterPath, title;
-  final dynamic voteAverage;
+  final String posterPath, title, releaseDate;
+  final dynamic voteAverage, backdropPath;
 
-  Popular.fromJson(Map<String, dynamic> json)
+  MovieInfo.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         posterPath = json['poster_path'],
+        backdropPath = json['backdrop_path'],
         title = json['title'],
+        releaseDate = json['release_date'],
         voteAverage = json['vote_average'];
 }
 
@@ -422,36 +627,32 @@ class ApiService {
       "https://movies-api.nomadcoders.workers.dev/movie";
   static const String imgUrl = "https://image.tmdb.org/t/p/w500/";
 
-  static Future<List<Popular>> getPopular() async {
-    List<Popular> populars = [];
-    final url = Uri.parse(popularUrl);
+  static Future<List<MovieInfo>> getMovieInfos(String whatInfo) async {
+    String findUrl;
+
+    switch (whatInfo) {
+      case "popular":
+        findUrl = popularUrl;
+        break;
+      case "playing":
+        findUrl = nowPlayingUrl;
+        break;
+      case "coming":
+        findUrl = comingSoonUrl;
+        break;
+      default:
+        throw Error();
+    }
+
+    List<MovieInfo> movieInfos = [];
+    final url = Uri.parse(findUrl);
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final List<dynamic> movies = jsonDecode(response.body)['results'];
       for (var movie in movies) {
-        populars.add(Popular.fromJson(movie));
+        movieInfos.add(MovieInfo.fromJson(movie));
       }
-      return populars;
-    }
-    throw Error();
-  }
-
-  void getNowPlaying() async {
-    final url = Uri.parse(nowPlayingUrl);
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      print(response.body);
-      return;
-    }
-    throw Error();
-  }
-
-  void getComingSoon() async {
-    final url = Uri.parse(comingSoonUrl);
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      return movieInfos;
     }
     throw Error();
   }
